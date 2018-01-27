8343c24a16f679ef559854c901ce535cd2bcdba6
Resay2k <yaser@ualberta.ca>
2018-01-27 13:11:45 -0700

    created sign in page and button

---
+++ Android/app/src/main/java/com/example/android/ranchat/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.ranchat;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Ydot on 2018-01-27.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        public void signIn(View v) {
+
+    }
+}
