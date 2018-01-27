6bf5dce16b0bae4376f72d45bcec485c356e6582
musavic <mustafa.qadir.mohamed@gmail.com>
2018-01-27 14:57:44 -0700

    Begin

--- Android/app/src/main/java/com/example/android/ranchat/LoginActivity.java
+++ Android/app/src/main/java/com/example/android/ranchat/LoginActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by Ydot on 2018-01-27.
@@ -11,12 +15,48 @@ import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        public void signIn(View v) {
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
 
+//        public void signIn(View v) {
+//        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressDialog();
+
+        TextView idView = findViewById(R.id.anonymous_status_id);
+        TextView emailView = findViewById(R.id.anonymous_status_email);
+        boolean isSignedIn = (user != null);
+
+        // Status text
+        if (isSignedIn) {
+            idView.setText(getString(R.string.id_fmt, user.getUid()));
+            emailView.setText(getString(R.string.email_fmt, user.getEmail()));
+        } else {
+            idView.setText(R.string.signed_out);
+            emailView.setText(null);
+        }
+
+        // Button visibility
+        findViewById(R.id.button_anonymous_sign_in).setEnabled(!isSignedIn);
+        findViewById(R.id.button_anonymous_sign_out).setEnabled(isSignedIn);
+        findViewById(R.id.button_link_account).setEnabled(isSignedIn);
     }
 }
